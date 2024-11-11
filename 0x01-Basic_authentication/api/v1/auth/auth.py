9d516dc84ab863145a10c9efa4834b6fe34f53a8
Oxshady <shadymahmod55@gmail.com>
2024-11-12 01:58:26 +0200

    Add Auth class with methods for authentication, authorization, and current user handling

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,16 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require authentication"""
+        pass
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header"""
+        pass
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        pass
