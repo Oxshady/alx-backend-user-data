f30b11247843a773bfad03fd7a22738ffce0347d
Oxshady <shadymahmod55@gmail.com>
2024-11-12 01:38:00 +0200

    Add route to trigger 401 Unauthorized error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route("/unauthorized", strict_slashes=False)
+def auth():
+  """unauthorized that raise 401 err"""
+  abort(401)
