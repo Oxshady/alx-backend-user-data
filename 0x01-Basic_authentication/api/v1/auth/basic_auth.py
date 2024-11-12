88af1cea480094ce633a9a61ffb0f36a3a825a80
Oxshady <shadymahmod55@gmail.com>
2024-11-12 17:04:57 +0200

    feat(auth): add method to extract Base64 part of Authorization header
    Implemented  to return the Base64-encoded
    credentials from the Authorization header in a Basic Authentication request.
    The method ensures the header is properly formatted and extracts the credentials.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,14 @@ class BasicAuth(Auth):
     placeholder for basic
     authentication features to be expanded in the future.
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ returns the Base64 part of the
+        Authorization header for a Basic Authentication"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
