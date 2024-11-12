14ca7f2d749252c274ad4a3f8f06e274b1b82b04
Oxshady <shadymahmod55@gmail.com>
2024-11-12 20:16:45 +0200

    fix: Resolve missing argument issue in current_user method
    - Modified current_user method in BasicAuth to correctly extract and pass both user_email and user_pwd to user_object_from_credentials.
    - Improved token extraction and handling logic to ensure all authentication steps work as intended.
    - Resolved TypeError due to missing user_pwd argument.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -89,6 +89,6 @@ class BasicAuth(Auth):
                 if token is not None:
                     token = self.extract_user_credentials(token)
                     if token is not None:
-                        user = self.user_object_from_credentials(token)
+                        user = self.user_object_from_credentials(token[0], [1])
                         return user
         return None
