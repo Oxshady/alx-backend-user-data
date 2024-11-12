92fd2e3ef9b00f3ef651707bafbdaa03fb3bc738
Oxshady <shadymahmod55@gmail.com>
2024-11-12 02:03:37 +0200

    adding type hinting

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,14 +7,14 @@ from typing import List, TypeVar
 
 class Auth:
     """Auth class"""
-    def require_auth(self, path: str, excluded_paths: List[str]):
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authentication"""
         pass
 
-    def authorization_header(self, request=None):
+    def authorization_header(self, request=None) -> str:
         """authorization header"""
         pass
 
-    def current_user(self, request=None):
+    def current_user(self, request=None) -> TypeVar('User'):
         """current user"""
         pass
