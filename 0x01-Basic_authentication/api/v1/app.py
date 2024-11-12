340c43877b87ab1a36670d291676aa991abd167d
Oxshady <shadymahmod55@gmail.com>
2024-11-12 16:19:04 +0200

    feat(auth): add before_request handler for request authentication checks
    Implemented a  function to be run before all incoming requests in the Flask app. This function utilizes the  object to check if a request path requires authentication, verifies the presence of an authorization header, and ensures the current user is valid. Requests are aborted with a 401 status if the authorization header is missing and 403 if the current user is unauthorized.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -35,6 +41,24 @@ def unauthorized(error) -> str:
     error handler for unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.before_request
+def before_all_requests() -> None:
+    """run before all requests"""
+    if auth is None:
+        pass
+    if auth.require_auth(request.path, [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+        ]
+                        ):
+        pass
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user() is None:
+        abort(403)
 
 
 if __name__ == "__main__":
