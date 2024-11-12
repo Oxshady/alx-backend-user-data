c3715db56beabcd251182e4e779826b68e8c770e
Oxshady <shadymahmod55@gmail.com>
2024-11-12 17:19:53 +0200

    feat(auth): add method to decode Base64 authorization header
    Implemented  to decode a Base64 string from
    the Authorization header. The method handles potential errors and returns the
    decoded value in UTF-8 format or  if decoding fails.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,14 +11,32 @@ class BasicAuth(Auth):
     placeholder for basic
     authentication features to be expanded in the future.
     """
+
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """ returns the Base64 part of the
+        """returns the Base64 part of the
         Authorization header for a Basic Authentication"""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
             return None
-        if not authorization_header.startswith('Basic '):
+        if not authorization_header.startswith("Basic "):
             return None
-        return authorization_header.split(' ')[1]
+        return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        from base64 import b64decode
+        from binascii import Error
+
+        try:
+            from_b64 = b64decode(base64_authorization_header)
+            return from_b64.decode("UTF-8")
+        except Error as err:
+            return None
