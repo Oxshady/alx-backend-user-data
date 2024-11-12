f59653587825f822d4de2fa8d083f53e672fe2d8
Oxshady <shadymahmod55@gmail.com>
2024-11-12 02:39:48 +0200

    Implement require_auth logic in Auth class and add placeholders for authorization_header and current_user methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authentication"""
-        pass
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path:
+            if path.endswith('/'):
+                path = path[0:-1]
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
