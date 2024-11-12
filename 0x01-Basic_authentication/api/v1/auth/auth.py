e85afa0360d7c70889884a39ce8771c1be37de9c
Oxshady <shadymahmod55@gmail.com>
2024-11-12 02:01:01 +0200

    remove type hinting for return untill adding return

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-from typing import List, TypeVar
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]):
         """require authentication"""
         pass
 
-    def authorization_header(self, request=None) -> str:
+    def authorization_header(self, request=None):
         """authorization header"""
         pass
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None):
         """current user"""
         pass
