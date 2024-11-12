ad3d2f70cb4c49b2017cd8b2c200aba96421e1be
Oxshady <shadymahmod55@gmail.com>
2024-11-12 16:44:47 +0200

    feat(auth): add BasicAuth class as a placeholder for basic authentication
    Introduced the  class, inheriting from . Currently serves as a placeholder for future basic authentication features. This allows for easy extension of authentication mechanisms in the future.

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module for BasicAuth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class inherits from Auth and serves as a
+    placeholder for basic
+    authentication features to be expanded in the future.
+    """
+    pass
