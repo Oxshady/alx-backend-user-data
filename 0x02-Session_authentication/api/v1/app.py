4fbf5e94f61926fa05d73c575a41ffc17adbea11
Oxshady <shadymahmod55@gmail.com>
2024-11-14 00:39:45 +0200

    fixing logic error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,8 +62,9 @@ def before_all_requests() -> None:
         ]
                         ):
         return
-    if auth.authorization_header(
-            request) is None and auth.session_cookie(request):
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request=request) is None:
         abort(403)
