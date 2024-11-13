dbe4fc781d241377b7b50b870a755883176df323
Oxshady <shadymahmod55@gmail.com>
2024-11-14 00:20:53 +0200

    feat: Add method to retrieve user ID by session ID
    - Implement  to return the user ID associated with a given session ID
    - Handle cases where session ID is not provided or is of incorrect type

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,3 +17,11 @@ class SessionAuth(Auth):
         session_id = uuid4().hex
         self.user_id_by_session_id.update({session_id: user_id})
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """return user_if of user that have the session"""
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
