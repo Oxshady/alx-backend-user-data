a8b0ecf9b5b46db04cf2305ba7bd3c399eae70f3
Oxshady <shadymahmod55@gmail.com>
2024-11-14 00:30:09 +0200

    feat: Add method to retrieve session cookie from request - Implement session_cookie to extract the session cookie value from the request - Return 'None' if the request is not provided or the cookie is not found

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,7 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
-
+from os import getenv
 
 class Auth:
     """Auth class"""
@@ -32,3 +32,9 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """current user"""
         pass
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        return request.cookies.get(getenv("SESSION_NAME"))
