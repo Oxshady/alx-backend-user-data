878cc9923d40a287ee3b9b638faeea7823a6d384
Oxshady <shadymahmod55@gmail.com>
2024-11-20 16:29:50 +0200

    DB module: Define database connection and add_user method
    - Implemented DB class for managing SQLite database connection.
    - Added session management via SQLAlchemy's sessionmaker.
    - Defined add_user method to add new user entries to the database.
    - Ensured table creation and removal using SQLAlchemy's Base metadata.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,7 +28,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email, hashed_password):
         """add new user to db"""
         from user import User
