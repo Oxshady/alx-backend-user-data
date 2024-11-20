cf5fcc051b753bcd7cd4d257d5dd20ce1ddc6728
Oxshady <shadymahmod55@gmail.com>
2024-11-20 16:08:09 +0200

    Define User model with SQLAlchemy ORM
    - Created User model with fields for ID (Integer), email, hashed_password, session_id, and reset_token.
    - The id field is set as the primary key.
    - Ensured proper data types and nullable attributes for each column.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 This module defines the User model for a database using SQLAlchemy
 """
 from sqlalchemy import Integer, String, Column
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 base = declarative_base()
@@ -16,7 +16,7 @@ class User(base):
     """
     __tablename__ = 'users'
 
-    id = Column(String(255), primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
